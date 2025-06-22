2e63a2c3898b6a87142e119b66852dd42edbea86
adinfahru <mohamad.fahrudin18@gmail.com>
2025-06-22 22:07:32 +0700

    feat(auth): enhance Google OAuth authentication with user session handling

--- lib/session.ts
+++ lib/session.ts
@@ -1,8 +1,17 @@
-import { User, getServerSession } from "next-auth";
+import { Session, User, getServerSession } from "next-auth";
 import { authOptions } from "./auth";
 
 export const getUserSession = async (): Promise<User> => {
   const authUserSession = await getServerSession(authOptions);
   if (!authUserSession) throw new Error("unauthorized");
   return authUserSession.user as User;
+};
+
+export const getSession = async (): Promise<Session | null> => {
+  return await getServerSession(authOptions);
+};
+
+export const isAuthenticated = async (): Promise<boolean> => {
+  const session = await getSession();
+  return session !== null;
 };
